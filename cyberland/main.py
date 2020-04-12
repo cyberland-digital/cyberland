89fbce2c7a2389d15b9a6c807b2406b1fa86db3f
James <jstone@jnet-it.com>
2020-04-12 15:42:50 +0100

    prototype database schema

--- cyberland/main.py
+++ cyberland/main.py
@@ -1,9 +1,24 @@
 from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
-from config import DevConfig
+from .config import DevConfig
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.sqlite'
+
+db = SQLAlchemy(app)
+
+
+class board(db.Model):
+    id = db.Column('board_id', db.Integer, primary_key=True)
+    name = db.Column(db.String(1))
+
+
+class posts(db.Model):
+    id = db.Column('post_id', db.Integer, primary_key=True)
+    content = db.Column('post_content', db.String(10000))
+    reply_to = db.Column('reply_to', db.ForeignKey(), nullable=True)
 
 
 @app.route('/')
