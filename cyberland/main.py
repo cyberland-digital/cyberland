5f3f23d6f9f9587877e2b972038952e6e0c55669
Joe <joe@joeth.uk>
2020-04-12 14:25:06 +0100

    add config.py

--- cyberland/main.py
+++ cyberland/main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from config import DevConfig
 
 app = Flask(__name__)
 
@@ -7,6 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
 @app.route("/tut.txt") 
 def tut():
     return render_template("tut.html")
