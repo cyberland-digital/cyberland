572bd786a00f69fc44b626a0b97e428ad523370c
James <jstone@jnet-it.com>
2020-04-13 18:50:46 +0100

    add images

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -2,7 +2,7 @@ drop table if exists news;
 create table news (
     id integer primary key autoincrement,
     created timestamp not null default current_timestamp,
-    bumpCount int default null,
+    bumpCount int default 0,
     content text not null,
     replyTo integer default null,
 
@@ -15,7 +15,7 @@ drop table if exists tech;
 create table tech (
     id integer primary key autoincrement,
     created timestamp not null default current_timestamp,
-    bumpCount int default null,
+    bumpCount int default 0,
     content text not null,
     replyTo integer default null,
 
@@ -28,11 +28,24 @@ drop table if exists offtopic;
 create table offtopic (
     id integer primary key autoincrement,
     created timestamp not null default current_timestamp,
-    bumpCount int default null,
+    bumpCount int default 0,
     content text not null,
     replyTo integer default null,
 
     check (
         length("content") <= 10000
+    )
+);
+
+drop table if exists images;
+create table images (
+    id integer primary key autoincrement,
+    created timestamp not null default current_timestamp,
+    bumpCount int default 0,
+    content text not null,
+    replyTo integer default null,
+
+    check (
+        length("content") <= 100000
     )
 );
