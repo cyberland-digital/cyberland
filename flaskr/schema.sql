96b14864f842453ef727a97c72b0833c65208bd6
Joe <joe@joeth.uk>
2020-04-15 10:13:49 +0100

    add /c/

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -49,3 +49,16 @@ create table images (
         length("content") <= 100000
     )
 );
+
+drop table if exists tests;
+create table tests (
+    id integer primary key autoincrement,
+    time timestamp not null default current_timestamp,
+    bumpCount int default 0,
+    content text not null,
+    replyTo integer default null,
+
+    check (
+        length("content") <= 100000
+    )
+);
