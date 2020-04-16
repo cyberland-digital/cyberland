49a3ccb0b3878fd5ebf06eeb64cc16acdb18c712
James <jstone@jnet-it.com>
2020-04-16 23:25:39 +0100

    Overhaul schema

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,64 +1,27 @@
-drop table if exists news;
-create table news (
-    id integer primary key autoincrement,
-    time timestamp not null default current_timestamp,
-    bumpCount int default 0,
-    content text not null,
-    replyTo integer default 0 not null,
-
-    check (
-        length("content") <= 10000
-    )
+drop table if exists site_boards;
+create table if not exists site_boards
+(
+    id   serial primary key,
+    name varchar (15) unique ,
+    slug varchar(2) unique ,
+    character_limit integer not null default 1000,
+    federated boolean
 );
 
-drop table if exists tech;
-create table tech (
-    id integer primary key autoincrement,
-    time timestamp not null default current_timestamp,
-    bumpCount int default 0,
-    content text not null,
-    replyTo integer default 0 not null,
-
-    check (
-        length("content") <= 10000
-    )
+drop table if exists site_posts;
+create table if not exists site_posts
+(
+    id        serial primary key,
+    board_id  integer references site_boards(id),
+    content   text,
+    bumpCount integer default 0,
+    replyTo   integer default 0,
+    time      timestamp default now()
 );
 
-drop table if exists offtopic;
-create table offtopic (
-    id integer primary key autoincrement,
-    time timestamp not null default current_timestamp,
-    bumpCount int default 0,
-    content text not null,
-    replyTo integer default 0 not null,
 
-    check (
-        length("content") <= 10000
-    )
-);
-
-drop table if exists images;
-create table images (
-    id integer primary key autoincrement,
-    time timestamp not null default current_timestamp,
-    bumpCount int default 0,
-    content text not null,
-    replyTo integer default 0 not null,
-
-    check (
-        length("content") <= 100000
-    )
-);
-
-drop table if exists tests;
-create table tests (
-    id integer primary key autoincrement,
-    time timestamp not null default current_timestamp,
-    bumpCount int default 0,
-    content text not null,
-    replyTo integer default 0 not null,
-
-    check (
-        length("content") <= 500
-    )
-);
+insert into site_boards (name, slug, character_limit, federated) values ('technology', 't', 10000, false);
+insert into site_boards (name, slug, character_limit, federated) values ('offtopic', 'o', 10000, false);
+insert into site_boards (name, slug, character_limit, federated) values ('client test', 'c', 10000, false);
+insert into site_boards (name, slug, character_limit, federated) values ('images', 'i', 100000, false);
+insert into site_boards (name, slug, character_limit, federated) values ('news', 'n', 500, false);
